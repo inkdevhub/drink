f08a77a09649b588c1fc7fa347d26c054dd5a399
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-06-16 12:31:27 +0200

    Building contract

--- drink-cli/src/cli.rs
+++ drink-cli/src/cli.rs
@@ -2,8 +2,12 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub enum CliCommand {
+    #[clap(alias = "b")]
     Build,
+    #[clap(alias = "d")]
     Deploy,
+    #[clap(alias = "c")]
     Call,
+    #[clap(alias = "e")]
     Exit,
 }
--- drink-cli/src/main.rs
+++ drink-cli/src/main.rs
@@ -1,13 +1,40 @@
-use std::io;
+use std::{env, io, process::Command};
+
 use clap::Parser;
 use cli::CliCommand;
 
 mod cli;
 
+const CONTRACT_DIR: &'static str = "../example/";
+
+fn build_contract() {
+    println!("Building contract...");
+
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let contract_path = current_dir.join(CONTRACT_DIR);
+    env::set_current_dir(contract_path).expect("Failed to change directory");
+
+    let output = Command::new("cargo-contract")
+        .arg("contract")
+        .arg("build")
+        .arg("--release")
+        .output()
+        .expect("Failed to execute 'cargo contract' command");
+
+    if output.status.success() {
+        println!("Contract built successfully");
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Error executing 'cargo contract' command:\n{}", stderr);
+    }
+}
+
 fn main() {
     loop {
         let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("Failed to get user input");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to get user input");
 
         let cli_command = match CliCommand::try_parse_from(&["", user_input.trim()]) {
             Ok(cli_command) => cli_command,
@@ -19,7 +46,7 @@ fn main() {
 
         match cli_command {
             CliCommand::Build => {
-                println!("Build");
+                build_contract();
             }
             CliCommand::Deploy => {
                 println!("Deploy");
