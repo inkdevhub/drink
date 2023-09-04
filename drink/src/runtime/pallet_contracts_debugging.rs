c2092cfdddb22c46c48838bf246875362e592c0c
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-09-04 12:10:23 +0200

    Add heavy machinery (runtime interface and externalities)

--- drink/src/runtime/pallet_contracts_debugging.rs
+++ drink/src/runtime/pallet_contracts_debugging.rs
@@ -1,9 +1,34 @@
 use pallet_contracts::debug::{CallSpan, ExportedFunction, Tracing};
 use pallet_contracts_primitives::ExecReturnValue;
+use sp_externalities::{decl_extension, ExternalitiesExt};
+use sp_runtime_interface::runtime_interface;
 
 use crate::runtime::Runtime;
 
 type CodeHash<R> = <R as frame_system::Config>::Hash;
+
+pub trait DebugExtT {
+    fn after_call(&self, code_hash: Vec<u8>, is_call: bool, input_data: Vec<u8>, result: Vec<u8>);
+}
+
+decl_extension! {
+    pub struct DebugExt(Box<dyn DebugExtT + Send>);
+}
+
+#[runtime_interface]
+pub trait ContractCallDebugger {
+    fn after_call(
+        &mut self,
+        code_hash: Vec<u8>,
+        is_call: bool,
+        input_data: Vec<u8>,
+        result: Vec<u8>,
+    ) {
+        self.extension::<DebugExt>()
+            .expect("Failed to find `DebugExt` extension")
+            .after_call(code_hash, is_call, input_data, result);
+    }
+}
 
 pub enum DrinkDebug {}
 
@@ -29,8 +54,14 @@ pub struct DrinkCallSpan<CodeHash> {
     pub input_data: Vec<u8>,
 }
 
-impl<CodeHash> CallSpan for DrinkCallSpan<CodeHash> {
+impl<CodeHash: AsRef<[u8]>> CallSpan for DrinkCallSpan<CodeHash> {
     fn after_call(self, output: &ExecReturnValue) {
-        todo!()
+        let raw_code_hash: &[u8] = self.code_hash.as_ref();
+        contract_call_debugger::after_call(
+            raw_code_hash.to_vec(),
+            matches!(self.entry_point, ExportedFunction::Call),
+            self.input_data.to_vec(),
+            output.data.clone(),
+        );
     }
 }
