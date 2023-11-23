a7d5ec9a332ed0dc5f913154696e1d0985463401
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-11-23 14:16:34 +0100

    Runtime interaction example (#87)

---
+++ examples/runtime-interaction/README.md
@@ -0,0 +1,27 @@
+# Runtime interaction
+
+This example shows how we can easily send transactions to a blockchain, just like with `subxt` or a similar client.
+
+This way we can leverage `drink!` to a full-scope runtime simulation engine.
+
+## Running
+
+```bash
+cargo test --release
+```
+
+## `drink::Sandbox` vs `drink::Session`
+
+While in most examples and showcases for `drink` you will see `drink::Session`, here we are using `drink::Sandbox`.
+`Session` is very useful when you are working with contracts, but if you are focusing only on the runtime interaction, `Sandbox` is enough.
+You can always switch from `Session` to `Sandbox` with:
+```rust
+    let session = Session::<Runtime>::new();
+    ...
+    let sandbox = session.sandbox(); // `sandbox` has type `&mut Sandbox<Runtime>`
+```
+
+`Sandbox` is just a runtime wrapper, which enables you to interact directly with the runtime.
+On the other hand, `Session` is a wrapper around `Sandbox` which also provides a useful context for working with contracts.
+
+A rule of thumb is: if you are working with contracts, use `Session`, otherwise use `Sandbox`.
---
+++ examples/runtime-interaction/lib.rs
@@ -0,0 +1,91 @@
+#[cfg(test)]
+mod tests {
+    use drink::{
+        pallet_balances, pallet_contracts,
+        pallet_contracts::Determinism,
+        runtime::{minimal::RuntimeCall, MinimalRuntime, Runtime},
+        AccountId32, Sandbox,
+    };
+
+    #[test]
+    fn we_can_make_a_token_transfer_call() {
+        // We create a sandbox object, which represents a blockchain runtime.
+        let mut sandbox = Sandbox::<MinimalRuntime>::new().expect("Failed to create sandbox");
+
+        // Bob will be the recipient of the transfer.
+        const BOB: AccountId32 = AccountId32::new([2u8; 32]);
+
+        // Firstly, let us check that the recipient account (`BOB`) is not the default actor, that
+        // will be used as the caller.
+        assert_ne!(MinimalRuntime::default_actor(), BOB);
+
+        // Recipient's balance before the transfer.
+        let initial_balance = sandbox.free_balance(&BOB);
+
+        // Prepare a call object, a counterpart of a blockchain transaction.
+        let call_object = RuntimeCall::Balances(pallet_balances::Call::transfer {
+            dest: BOB,
+            value: 100,
+        });
+
+        // Submit the call to the runtime.
+        sandbox
+            .runtime_call(call_object, Some(MinimalRuntime::default_actor()))
+            .expect("Failed to execute a call");
+
+        // In the end, the recipient's balance should be increased by 100.
+        assert_eq!(sandbox.free_balance(&BOB), initial_balance + 100);
+    }
+
+    #[test]
+    fn we_can_work_with_the_contracts_pallet_in_low_level() {
+        let mut sandbox = Sandbox::<MinimalRuntime>::new().expect("Failed to create sandbox");
+
+        // A few runtime calls are also available directly from the sandbox. This includes a part of
+        // the contracts API.
+        let upload_result = sandbox
+            .upload_contract(
+                wat::parse_str(CONTRACT).unwrap(),
+                MinimalRuntime::default_actor(),
+                None,
+                Determinism::Enforced,
+            )
+            .expect("Failed to upload a contract");
+
+        // If a particular call is not available directly in the sandbox, it can always be executed
+        // via the `runtime_call` method.
+        let call_object = RuntimeCall::Contracts(pallet_contracts::Call::remove_code {
+            code_hash: upload_result.code_hash,
+        });
+
+        sandbox
+            .runtime_call(call_object, Some(MinimalRuntime::default_actor()))
+            .expect("Failed to remove a contract");
+    }
+
+    /// This is just a dummy contract code, that does nothing. It is written in WAT, a text format
+    /// for WebAssembly. We need to have some valid contract bytes in order for `upload_contract`
+    /// to succeed.
+    const CONTRACT: &str = r#"
+    (module
+	(import "seal0" "seal_deposit_event" (func $seal_deposit_event (param i32 i32 i32 i32)))
+	(import "seal0" "seal_return" (func $seal_return (param i32 i32 i32)))
+	(import "env" "memory" (memory 1 1))
+
+	(func (export "deploy"))
+
+	(func (export "call")
+    (call $seal_deposit_event
+      (i32.const 0)
+      (i32.const 0)
+      (i32.const 8)
+      (i32.const 4)
+    )
+		(call $seal_return
+			(i32.const 0)
+			(i32.const 0)
+			(i32.const 4)
+		)
+	)
+)"#;
+}
