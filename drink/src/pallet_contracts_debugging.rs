8a4ad3fac146c3cb0a840185414926cb1e6f5b31
Michael Mueller <mich@elmueller.net>
2024-11-28 10:46:30 +0100

    Apply `clippy` suggestions

--- drink/src/pallet_contracts_debugging.rs
+++ drink/src/pallet_contracts_debugging.rs
@@ -5,11 +5,11 @@
 //!
 //! The interaction between end-user and runtime is as follows:
 //! 1. At some points during execution, the pallet invokes some callback through its configuration
-//! parameter `Debug`.
+//!    parameter `Debug`.
 //! 2. In order to forward the callback outside the runtime, `Debug` will call a runtime interface,
-//! that will then forward the call further to the proper runtime extension.
+//!    that will then forward the call further to the proper runtime extension.
 //! 3. The runtime extension can be fully controlled by the end-user. It just has to be registered
-//! in the runtime.
+//!    in the runtime.
 //!
 //! So, in brief: pallet-contracts -> runtime interface -> runtime extension
 //!              |<-----------runtime side-------------->|<---user side--->|
